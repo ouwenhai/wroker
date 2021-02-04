89ad4a8502cca56c296999d000513afb27d03da4
ouwenhai <46631387+ouwenhai@users.noreply.github.com>
2021-02-04 17:32:26 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-$site_name = ""; //填入你的站点名，例如 https://214214.xyz/tools/dlink/
-$cf_worker = ""; //填入搭建的cf-worker链接,如 https://gd.kway.workers.dev/, 否则将默认使用作者所搭建的
+$site_name = "https://download.ouwenhai.tk/"; //填入你的站点名，例如 https://214214.xyz/tools/dlink/
+$cf_worker = "https://shy-recipe-1ee0.ouwenhai1225.workers.dev; //填入搭建的cf-worker链接,如 https://gd.kway.workers.dev/, 否则将默认使用作者所搭建的
 ?>
